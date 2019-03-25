4fe387be539279f939e2f7483e89bd1c3bd9c0e2
Adam Gibbons <adam.p.gibbons@gmail.com>
2019-03-25 21:24:14 +0000

    Imported messages for bug view page

--- tickets/views.py
+++ tickets/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 from .models import Ticket
 from comments.models import Comment
