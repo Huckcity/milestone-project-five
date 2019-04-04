07216803f6d1102f3e64b682038a6355693c2ddb
Huckcity <adam.p.gibbons@gmail.com>
2019-04-04 14:42:48 +0100

    Heroku debugging

--- unicornattractor/settings.py
+++ unicornattractor/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
@@ -82,17 +82,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'unicornattractor.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 if DEBUG:
     DATABASES = {
