11f9f0cbc027e0dc8077745061d2775afb647c5d
Huckcity <adam.p.gibbons@gmail.com>
2019-04-07 20:23:56 +0100

    Static url fix

--- unicornattractor/settings.py
+++ unicornattractor/settings.py
@@ -173,7 +173,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 
 AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = "https://%s/%s/static" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 DEFAULT_FILE_STORAGE = 'storage_backends.MediaStorage'
 
