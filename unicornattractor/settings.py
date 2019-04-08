68b0ebe57b396f9a9a931c68ea4cc93fb7e42af9
Huckcity <adam.p.gibbons@gmail.com>
2019-04-08 20:52:23 +0100

    Reverts back to dynamically selecting database

--- unicornattractor/settings.py
+++ unicornattractor/settings.py
@@ -91,7 +91,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'unicornattractor.wsgi.application'
 
-if not DEBUG:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
