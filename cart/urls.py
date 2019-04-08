75f3237a586b402c4e6c87cbc67edc40ed7ff5a7
Huckcity <adam.p.gibbons@gmail.com>
2019-04-08 18:08:46 +0100

    Adds update and remove item functionality to cart

--- cart/urls.py
+++ cart/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.view_cart, name='cart'),
     url(r'^add/(?P<featureid>\d+)/$', views.addtocart, name='addtocart'),
-    # url(r'^remove/(?P<featureid>\d+)/$', views.removefromcart, name='removefromcart'),
+    url(r'^remove/(?P<featureid>\d+)/$', views.removefromcart, name='removefromcart'),
+    url(r'^updatecart/(?P<featureid>\d+)/$', views.updatecart, name='updatecart'),
     url(r'^checkout$', views.checkout, name='checkout'),
     url(r'^charge$', views.charge, name='charge'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,15 @@
 import stripe
 
+from decimal import *
 from django.conf import settings
 
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+
+from tickets.models import Contribution
+from tickets.views import Ticket
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 def view_cart(request):
@@ -18,8 +24,6 @@ def addtocart(request, featureid):
 
     cart = request.session.get('cart', {})
     if featureid not in cart:
-
-        print(request.POST['contribution_amount'])
 
         cart[featureid] = {
             'id': featureid,
@@ -65,10 +69,71 @@ def checkout(request):
 def charge(request):
 
     if request.method == 'POST':
-        charge = stripe.Charge.create(
-            amount=request.POST['data-amount'],
-            currency='usd',
-            description='A Django charge',
-            source=request.POST['stripeToken']
-        )
-        return render(request, 'charge.html')
+
+        cart = request.session.get('cart', {})
+        total_charge = 0
+
+        for item in cart.keys():
+
+            amount = cart[item]['contrib_amount']
+            total_charge += Decimal(amount)
+
+        try:
+
+            charge = stripe.Charge.create(
+                amount=int(total_charge*100),
+                currency='eur',
+                description='UA Feature Contribution',
+                source=request.POST['stripeToken']
+            )
+
+            if charge.paid:
+
+                for item in cart.keys():
+
+                    user = request.user
+                    ticket = get_object_or_404(Ticket, pk=item)
+                    amount = cart[item]['contrib_amount']
+
+                    contribution = Contribution(
+                        userid=user,
+                        ticket=ticket,
+                        amount=amount)
+
+                    contribution.save()
+
+                messages.success(
+                    request,
+                    'Thank you for your contribution, \
+                    keep an eye on the features to see \
+                    when they will begin development.')
+
+                request.session['cart'] = {}
+                return redirect(reverse('dashboard'))
+
+            else:
+
+                messages.error(
+                    request, 'The payment gateway declined your card.')
+
+        except stripe.error.CardError:
+            messages.error(
+                request, 'There was an error processing your payment.')
+
+        return redirect('dashboard')
+
+
+def updatecart(request, featureid):
+    """
+    Delete a feature contribution from the cart
+    """
+    cart = request.session.get('cart', {})
+
+    if featureid in cart:
+
+        cart[featureid]['contrib_amount'] = request.POST['contribution_amount']
+        messages.success(request, "Cart Updated!")
+
+    request.session['cart'] = cart
+
+    return redirect(reverse('cart'))
