440a52dc826ccb09283d4fc04f03e5e5047d8204
Huckcity <adam.p.gibbons@gmail.com>
2019-04-19 18:17:29 +0100

    Re add tests now that travis bug is rectified

--- tickets/tests.py
+++ tickets/tests.py
@@ -1,58 +1,58 @@
-# from django.test import TestCase
-# from django.contrib.auth.models import User
-# from django.core.exceptions import ValidationError
-# from .models import Ticket
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from .models import Ticket
 
 
-# class UserTest(TestCase):
+class UserTest(TestCase):
 
-#     @classmethod
-#     def setUpTestData(self):
-#         # Set up non-modified objects used by all test methods
-#         self.user = User.objects.create(
-#             username='user123', email='email@email.com', password='password1')
+    @classmethod
+    def setUpTestData(self):
+        # Set up non-modified objects used by all test methods
+        self.user = User.objects.create(
+            username='user123', email='email@email.com', password='password1')
 
-#         self.bug_ticket = Ticket.objects.create(userid=self.user,
-#                                                 title='test bug title',
-#                                                 description='test feature description',
-#                                                 type='Bug')
+        self.bug_ticket = Ticket.objects.create(userid=self.user,
+                                                title='test bug title',
+                                                description='test feature description',
+                                                type='Bug')
 
-#         self.feature_ticket = Ticket.objects.create(userid=self.user,
-#                                                     title='test feature title',
-#                                                     description='test feature description',
-#                                                     type='Feature')
+        self.feature_ticket = Ticket.objects.create(userid=self.user,
+                                                    title='test feature title',
+                                                    description='test feature description',
+                                                    type='Feature')
 
-#     def test_create_bug_report(self):
+    def test_create_bug_report(self):
 
-#         user = self.user
-#         new_ticket = Ticket.objects.create(userid=user,
-#                                            title='test bug title',
-#                                            description='test bug description',
-#                                            type='Bug')
+        user = self.user
+        new_ticket = Ticket.objects.create(userid=user,
+                                           title='test bug title',
+                                           description='test bug description',
+                                           type='Bug')
 
-#         self.assertEqual(new_ticket.title, 'test bug title')
+        self.assertEqual(new_ticket.title, 'test bug title')
 
-#     def test_create_feature_request(self):
+    def test_create_feature_request(self):
 
-#         user = self.user
-#         new_ticket = Ticket.objects.create(userid=user,
-#                                            title='test feature title',
-#                                            description='test feature description',
-#                                            type='Feature')
+        user = self.user
+        new_ticket = Ticket.objects.create(userid=user,
+                                           title='test feature title',
+                                           description='test feature description',
+                                           type='Feature')
 
-#         self.assertEqual(new_ticket.title, 'test feature title')
+        self.assertEqual(new_ticket.title, 'test feature title')
 
-#     def test_create_ticket_without_type(self):
+    def test_create_ticket_without_type(self):
 
-#         user = self.user
-#         new_ticket = Ticket.objects.create(userid=user,
-#                                            title='test feature title',
-#                                            description='test feature description')
+        user = self.user
+        new_ticket = Ticket.objects.create(userid=user,
+                                           title='test feature title',
+                                           description='test feature description')
 
-#         self.assertRaises(ValidationError, new_ticket.full_clean)
+        self.assertRaises(ValidationError, new_ticket.full_clean)
 
-#     def test_create_ticket_has_default_status_pending(self):
+    def test_create_ticket_has_default_status_pending(self):
 
-#         new_ticket = self.bug_ticket
+        new_ticket = self.bug_ticket
 
-#         self.assertEqual(new_ticket.status, 'Pending')
+        self.assertEqual(new_ticket.status, 'Pending')
